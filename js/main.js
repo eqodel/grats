b38f244af7a8f75104bfe36c6f56b59b2e1cbf12
eqodel <eqodel@mail.ru>
2022-11-02 21:01:40 +0300

    create: slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper__button--next',
+      prevEl: '.swiper__button--prev',
+    }
+
+  });
