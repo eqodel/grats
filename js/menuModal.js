756123998a0b24aba693510e5dd8378d753acb76
eqodel <eqodel@mail.ru>
2022-12-16 15:29:59 +0300

    create: overflow for body

--- js/menuModal.js
+++ js/menuModal.js
@@ -3,5 +3,8 @@ const menuBtn = document.querySelector('.menu')
 menuBtn.addEventListener('click', (e) => {
     e.preventDefault()
     const menu = document.querySelector('.mobile-menu')
+    const body = document.querySelector('body')
+    body.classList.toggle('body--active')
     menu.classList.toggle('menu--active')
+    
 })
