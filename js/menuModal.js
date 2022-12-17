ece67b2c29eaad4d4c56f45c48a91b1c5deb88c2
eqodel <eqodel@mail.ru>
2022-12-17 10:43:25 +0300

    create: script for swipe menu

--- js/menuModal.js
+++ js/menuModal.js
@@ -1,10 +1,28 @@
 const menuBtn = document.querySelector('.menu')
+const menu = document.querySelector('.mobile-menu')
+const body = document.querySelector('body')
 
-menuBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const menu = document.querySelector('.mobile-menu')
-    const body = document.querySelector('body')
-    body.classList.toggle('body--active')
-    menu.classList.toggle('menu--active')
+
+
+menu.addEventListener("touchstart", touchStart, false);
+menu.addEventListener("touchmove", touchMove, false);
+
+let x1 = null;
+
+function touchStart (event) {
+    const touch = event.touches[0]
+    x1 = touch.clientX
+}
+
+function touchMove (event) {
+    if (!x1) {
+        return false;
+    }
+
+    let x2 = event.touches[0].clientX;
+
+    let xDiff = x2 - x1;
     
-})
+    if (Math.abs(xDiff > 0)) menu.style.transform = 'translateX(0%)'
+    else menu.style.transform = 'translateX(-100%)'
+}
